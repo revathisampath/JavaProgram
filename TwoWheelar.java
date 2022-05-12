dc741b7207c7781aa229da8593b05e46ec1ce51f
revathisampath <104840865+revathisampath@users.noreply.github.com>
2022-05-12 10:36:35 +0530

    Add files via upload

---
+++ TwoWheelar.java
@@ -0,0 +1,22 @@
+package vechicle;
+
+public class TwoWheelar {
+	 String bikeName="yamaha";
+	 int noOfWheels=4;
+	 short noOfMirrors=3;
+	 long chassisNumber=234567891782l;
+	 double runningKm=120;
+	 boolean isPunctured=true;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TwoWheelar object=new TwoWheelar();
+		System.out.println(object.bikeName);
+		System.out.println(object.noOfMirrors);
+		System.out.println(object.chassisNumber);
+		System.out.println(object.runningKm);
+		System.out.println(object.isPunctured);
+		
+
+	}
+
+}
